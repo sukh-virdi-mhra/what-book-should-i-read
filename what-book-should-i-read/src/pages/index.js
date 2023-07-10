2c6535fcd51684ef81da396068a6d0f7353752b9
Sukh Virdi <sukh.virdi2@mhra.gov.uk>
2023-07-10 19:24:34 +0100

    add routing

--- what-book-should-i-read/src/pages/index.js
+++ what-book-should-i-read/src/pages/index.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const HomePage = () => {
+  const router = useRouter();
+
+  const handleStart = () => {
+    router.push("/question");
+  };
+
   return (
     <div>
       <h1>Welcome to "What Book Should I Read?"</h1>
       <p>Answer a few questions to discover your next great read!</p>
-      <button>Start Questionnaire</button>
+      <button onClick={handleStart}>Start Questionnaire</button>
     </div>
   );
 };
