4f3ecfc688761a654ff812e3f158bd4e7c8d8a38
Sukh Virdi <sukh.virdi2@mhra.gov.uk>
2023-07-10 19:22:24 +0100

    start writing question component

---
+++ what-book-should-i-read/src/pages/question.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const QuestionPage = () => {
+  return (
+    <div>
+      <h2>Question 1</h2>
+      <p>What genre are you in the mood for?</p>
+      <ul>
+        <li>Option 1</li>
+        <li>Option 2</li>
+        <li>Option 3</li>
+      </ul>
+      <button>Next</button>
+    </div>
+  );
+};
+
+export default QuestionPage;
