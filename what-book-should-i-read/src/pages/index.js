a38bcc049b05ee361c22ca49d122dd6ed3ddd5cb
Sukh Virdi <sukh.virdi2@mhra.gov.uk>
2023-07-10 19:21:48 +0100

    create pages folder and homepage

---
+++ what-book-should-i-read/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to "What Book Should I Read?"</h1>
+      <p>Answer a few questions to discover your next great read!</p>
+      <button>Start Questionnaire</button>
+    </div>
+  );
+};
+
+export default HomePage;
