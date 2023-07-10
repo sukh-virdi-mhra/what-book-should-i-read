a4da814d7d6cdb29457692bc0cefaf632a25dcc9
Sukh Virdi <sukh.virdi2@mhra.gov.uk>
2023-07-10 19:22:47 +0100

    create result page

---
+++ what-book-should-i-read/src/pages/result.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ResultPage = () => {
+  return (
+    <div>
+      <h2>Book Recommendation</h2>
+      <h3>Book Title</h3>
+      <p>Author: Author Name</p>
+      <p>Description: Book description goes here.</p>
+      <button>Start Over</button>
+    </div>
+  );
+};
+
+export default ResultPage;
